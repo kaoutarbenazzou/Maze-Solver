cab69afa73607f54989300efc3cb9508aa36c0a4
Kaoutar Benazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2023-06-12 13:05:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,3 +81,20 @@ class MazeSolver extends Grid{
         return found;
     }
 }
+
+var arr = [
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+]
+var maze = new MazeSolver(arr);
+console.log("check solavable -> source (0, 1) and Dest (4,3):")
+console.log(maze.solve(0,1,4,3));
+console.log("check solavable -> source (0, 0) and Dest (4,3):")
+console.log(maze.solve(0,0,4,3));
+console.log("check solavable -> source (0, 1) and Dest (3,3):")
+console.log(maze.solve(0,1,3,3));
+console.log("check solavable -> source (0, 1) and Dest (4,4):")
+console.log(maze.solve(0,1,4,4));
